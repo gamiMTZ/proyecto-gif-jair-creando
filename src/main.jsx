580842144715b2f5f100bf2154afacab849be115
gamiMTZ <gamalielpat@gami.mtz>
2023-12-04 17:32:25 -0600

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+//import { useState} from 'react';
+
+// Importamos la biblioteca de React
+import React from "react"; 
+// Importamos la biblioteca ReactDOM, que proporciona métodos específicos del DOM para React 
+import ReactDOM from "react-dom/client"; 
+// Importamos un componente llamado ProyectoGif desde otro archivo en el mismo directorio 
+import { ProyectoGif } from "./ProyectoGif"; 
+// Creamos un nuevo contenedor de raíz React en el elemento con el id 'root' 
+// y renderizamos el componente ProyectoGif en él 
+import './styles.css';
+ReactDOM.createRoot(document.getElementById("root")).render( 
+    // React.StrictMode es un componente especial de React que comprueba si tu aplicación tiene problemas potenciales durante el desarrollo
+    <React.StrictMode> 
+        {/* Aquí es donde se renderiza el componente ProyectoGif */} 
+        <ProyectoGif />
+    </React.StrictMode>
+);
